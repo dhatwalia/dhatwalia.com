5a33311ed5c10c93c87316f48339791ce161ad4a
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-04-18 23:14:09 -0400

    Create Home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Prajwal Dhatwalia</h1>
+    <p>
+        I am a Computer Science student from Lakehead University. I have worked as a Full Stack Developer at Surya Software. I have developed Batch Asset Liability Management(BALM) and Fund Transfer Pricing(FTP) software for Oman Arab Bank(OAB) and HDFC Bank using Rust. I have also developed a document generator using GatsbyJS.
+    </p>
+    <p>
+        I love to do pet projects when I am free, which includes (but is not limited to) Machine Learning, Mobile App Development, Linux Programming and Web Development.
+    </p>
+    <p>
+        I write my code in an organized, optimized and foolproof manner, keeping maintainability in mind. I use a hybrid of high-focus solo programming with collaborative teamwork for working on challenging projects. In a team with great diversity, I will learn from the passionate and experienced and mentor the energetic and curious.
+    </p>
+    </>
+);
+
+export default HomePage;
