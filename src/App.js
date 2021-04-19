3317d654726b854ad5f9161f0fcc01bd4bbab2de
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-04-19 00:08:23 -0400

    Create a 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
@@ -9,6 +10,7 @@ import PublicationPage from './pages/PublicationPage';
 import PublicationsListPage from './pages/PublicationsListPage';
 import VolunteerPage from './pages/VolunteerPage';
 import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -16,11 +18,14 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact/>
-          <Route path="/contact" component={ContactPage} />
-          <Route path="/publication/:name" component={PublicationPage} />
-          <Route path="/publications-list" component={PublicationsListPage} />
-          <Route path="/volunteer" component={VolunteerPage} />
+          <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/publication/:name" component={PublicationPage} />
+            <Route path="/publications-list" component={PublicationsListPage} />
+            <Route path="/volunteer" component={VolunteerPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
--- src/pages/PublicationPage.js
+++ src/pages/PublicationPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import publicationContent from './publication-content';
 import PublicationsList from '../components/PublicationsList';
+import NotFoundPage from './NotFoundPage';
 
 const PublicationPage = ({ match }) => {
     const name = match.params.name;
     const publication = publicationContent.find(publication => publication.name === name);
     const otherPublications = publicationContent.filter(publication => publication.name !== name);
+
+    if (!publication) return <NotFoundPage/>
 
     return (
         <>
