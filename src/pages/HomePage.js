9d73c7bee2f097103f1d896de78911d33cf733d0
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-04-21 01:01:52 -0400

    Update HomePage and VolunteerPage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 const HomePage = () => (
     <>
         <h1>Prajwal Dhatwalia</h1>
+        <img src={process.env.PUBLIC_URL + '/prajwal.jpg'} alt='prajwal-dhatwalia' class="image-60"/> 
         <p>
             I am a Computer Science student from Lakehead University. I have worked as a Full Stack Developer at Surya Software. I have developed Batch Asset Liability Management(BALM) and Fund Transfer Pricing(FTP) software for Oman Arab Bank(OAB) and HDFC Bank using Rust. I have also developed a document generator using GatsbyJS.
         </p>
--- src/pages/VolunteerPage.js
+++ src/pages/VolunteerPage.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const VolunteerPage = () => (
     <>
+
         <h1>Volunteer</h1>
+        <img src={process.env.PUBLIC_URL + '/helpage-india.png'} alt='help-age-india-logo' class="image-100"/> 
+        <p>
+            HelpAge India is a charity in India which works with and for disadvantaged elderly for nearly four decades. It has been running in India since 1978. There is an estimated 106 million elderly in India. HelpAge advocates for their needs, such as Universal Pension, quality healthcare and abuse faced by senior citizens. It advocates for elder-friendly policies and works towards their implementation. It works with Senior Citizens Associations to understand the needs of the disadvantaged elderly by working with and for them.
+        </p>
+        <p>
+            The aim is to help elders live active, dignified and healthier lives. HelpAge's focus over the years has moved from Welfare to Development.
+        </p>
+        <figure>
+        <img src={process.env.PUBLIC_URL + '/helping-elders.jpg'} alt='covid19-relief-india' class="image-80" />
+        <figcaption>Distribution of family Survival Kits for senior citizens in Kannigapuram, Chennai, India during COVID-19 pandemic.</figcaption>
+        </figure>
+        <p>
+            It provides elderly relief through various interventions for the elderly. It features one of the most extensive mobile healthcare programs through its Mobile Healthcare Units in India. It provides free healthcare services to destitute elders. It also helps elders earn their livelihood through the formation of Elder-Self-Help Groups, making them self-reliant. It conducts cataract surgeries to restore their sight. It's Support-a-Gran program looks after their basic needs of the elderly. It also features Elder Helplines across India. It provides relief and rehabilitation for elders post disasters. It provides active-ageing opportunities.
+        </p>
+        <a class="button" href="https://www.helpageindia.org/donate/" target="_blank">Donate</a>
     </>
 );
 
