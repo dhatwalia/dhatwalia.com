246a0b19bc4c658cfed7789802b9dadd7bc62b52
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-04-21 01:19:05 -0400

    Add contact information

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,6 +3,11 @@ import React from 'react';
 const ContactPage = () => (
     <>
         <h1>Contact</h1>
+        <p>Visit my LinkedIn profile 👉</p>
+        <a class="button" href="https://www.linkedin.com/in/prajwal-dhatwalia/" target="_blank">LinkedIn</a>
+        <br/>
+        <p>OR send me an email 👉</p>
+        <a class="button" href="mailto:dh.prajwal@gmail.com" target="_blank">Email</a>
     </>
 );
 
