d5b2a5f8235441e1662adc0d14e4da194e37942e
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-04-25 10:24:02 -0400

    Amend text content of Volunteer page

--- src/pages/VolunteerPage.js
+++ src/pages/VolunteerPage.js
@@ -6,7 +6,7 @@ const VolunteerPage = () => (
         <h1>Volunteer</h1>
         <img src={process.env.PUBLIC_URL + '/helpage-india.png'} alt='help-age-india-logo' class="image-100"/> 
         <p>
-            HelpAge India is a charity in India which works with and for disadvantaged elderly for nearly four decades. It has been running in India since 1978. There is an estimated 106 million elderly in India. HelpAge advocates for their needs, such as Universal Pension, quality healthcare and abuse faced by senior citizens. It advocates for elder-friendly policies and works towards their implementation. It works with Senior Citizens Associations to understand the needs of the disadvantaged elderly by working with and for them.
+            HelpAge India is a charity in India that has worked with and for disadvantaged elderly for nearly four decades. It has been running in India since 1978. There is an estimated 106 million elderly in India. HelpAge advocates for their needs, such as Universal Pension, quality healthcare and abuse faced by senior citizens. It advocates for elder-friendly policies and works towards their implementation. It also works with Senior Citizens Associations to understand the needs of the disadvantaged elderly by working with them.
         </p>
         <p>
             The aim is to help elders live active, dignified and healthier lives. HelpAge's focus over the years has moved from Welfare to Development.
@@ -16,7 +16,7 @@ const VolunteerPage = () => (
         <figcaption>Distribution of family Survival Kits for senior citizens in Kannigapuram, Chennai, India during COVID-19 pandemic.</figcaption>
         </figure>
         <p>
-            It provides elderly relief through various interventions for the elderly. It features one of the most extensive mobile healthcare programs through its Mobile Healthcare Units in India. It provides free healthcare services to destitute elders. It also helps elders earn their livelihood through the formation of Elder-Self-Help Groups, making them self-reliant. It conducts cataract surgeries to restore their sight. It's Support-a-Gran program looks after their basic needs of the elderly. It also features Elder Helplines across India. It provides relief and rehabilitation for elders post disasters. It provides active-ageing opportunities.
+            It provides elderly relief through various interventions for the elderly. It features one of the most extensive mobile healthcare programs through its Mobile Healthcare Units in India. It provides free healthcare services to destitute elders. It also helps elders earn their livelihood through the formation of Elder-Self-Help Groups, making them self-reliant. It conducts cataract surgeries to restore their sight. It's Support-a-Gran program looks after their basic needs. It also features Elder Helplines across India. It provides relief and rehabilitation for elders post disasters.
         </p>
         <a class="button" href="https://www.helpageindia.org/donate/" target="_blank">Donate</a>
     </>
