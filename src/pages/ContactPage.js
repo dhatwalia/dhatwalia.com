58bdf61778c901d1a8025473bd1c941d5ae2e698
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2024-03-11 19:07:07 -0400

    Updates to static QR code

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -7,12 +7,28 @@ const ContactPage = () => (
         <table width="100%" class="social">
             <tr>
                 <td class="tile">
+                    <a href="https://calendly.com/dhatwalia">
+                        <img src={process.env.PUBLIC_URL + "/icons/calendar.png"} class="icon" alt="calendly" />
+                    </a>
+                    <br />
+                    Book an appointment
+                </td>
+                <td class="tile">
                     <a href="mailto:dh.prajwal@gmail.com">
                         <img src={process.env.PUBLIC_URL + "/icons/email.png"} class="icon" alt="email" />
                     </a>
                     <br />
                     Email
                 </td>
+                <td class="tile">
+                    <a href="https://www.facebook.com/dh.prajwal">
+                        <img src={process.env.PUBLIC_URL + "/icons/facebook.png"} class="icon" alt="facebook" />
+                    </a>
+                    <br />
+                    Facebook
+                </td>
+            </tr>
+            <tr>
                 <td class="tile">
                     <a href="http://github.com/dhatwalia">
                         <img src={process.env.PUBLIC_URL + "/icons/github.png"} class="icon" alt="github" />
@@ -26,6 +42,13 @@ const ContactPage = () => (
                     </a>
                     <br />
                     LinkedIn
+                </td>
+                <td class="tile">
+                    <a href="http://twitter.com/dhatwalia97">
+                        <img src={process.env.PUBLIC_URL + "/icons/twitter.png"} class="icon" alt="twitter" />
+                    </a>
+                    <br />
+                    X (Twitter)
                 </td>
             </tr>
         </table>
