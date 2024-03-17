019834691a46a3b640fd2456a25f59a1d381b2a4
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2024-03-16 22:42:39 -0400

    Updates homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,13 +6,13 @@ const HomePage = () => (
         <h1>Prajwal</h1>
         <img src={process.env.PUBLIC_URL + '/prajwal.jpg'} alt='my-pic' class="image-60" />
         <p>
-            Hello, world! I am a Software Engineer with multiple years of software development experience in 6+ frameworks for clients across the globe.
+            Hello, world! I am a Full Stack Developer with 5+ years of experience in FinTech, CryptoTech, MarTech and Telematics using MERN stack, Machine Learning and Software Engineering.
         </p>
         <p>
-            I love to work with JavaScript, Python, Java and Rust. But I am willing to learn new technologies with the intention to master my skills.
+            I love to work with Rust, Typescript, Python and C++.
         </p>
         <p>
-            I write my code in an organized, optimized and foolproof manner, keeping maintainability in mind. I use a hybrid of high-focus solo programming with collaborative teamwork for working on challenging projects. In a team with great diversity, I will learn from the passionate and experienced and mentor the energetic and curious.
+            I can solve your problems with high throughput, scalable and maintainable technologies. I can lead development and wear multiple hats with product, design, development, testing, deployment and support teams in a collaborative environment. In a team with great diversity, I will learn from the passionate and experienced and mentor the energetic and curious.
         </p>
         <h2>Share this website</h2>
         <img src={process.env.PUBLIC_URL + '/qr.png'} alt='qr-code' class="image-40" />
