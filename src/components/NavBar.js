09199f2da716a1e2bb8e5289fec32090a56867b2
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2024-03-23 04:14:39 -0400

    Adds dark theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 
+function ToggleTheme() {
+    const temp = getComputedStyle(document.documentElement).getPropertyValue('--primary');
+    document.documentElement.style.setProperty('--primary', getComputedStyle(document.documentElement).getPropertyValue('--secondary'));
+    document.documentElement.style.setProperty('--secondary', temp);
+}
+
 const NavBar = () => (
     <nav>
         <ul>
             <img src={process.env.PUBLIC_URL + '/arrow.png'} alt='arrow' class="arrow" />
+            <li>
+                <img src={process.env.PUBLIC_URL + '/theme.png'} alt='theme' class="theme" onClick={ToggleTheme} />
+            </li>
             <li>
                 <Link to="/">Home<ScrollToTop /></Link>
             </li>
